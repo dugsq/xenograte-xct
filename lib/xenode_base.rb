76286c61e0966aca858de410f9f0994101689491
Yuan Hsueh <yuanhsueh@gmail.com>
2013-11-28 14:46:37 -0800

    change back to resolve_sys_dir()

--- lib/xenode_base.rb
+++ lib/xenode_base.rb
@@ -18,7 +18,7 @@ module XenoCore
         @disk_dir    = opts[:disk_dir]
         @log_path    = opts[:log_path]
         @tmp_dir     = opts[:tmp_dir]
-        @config      = apply_sys_token(opts[:config])
+        @config      = opts[:config]
         @debug       = opts[:config][:debug] if opts[:config]
         @loop_delay  = opts.fetch(:loop_delay, 5.0)
         @msg_count   = 0
@@ -88,31 +88,30 @@ module XenoCore
       end
     end
     
-    # deprecated by apply_sys_token
-    # def resolve_sys_dir(fp)
-    #   if fp
-    #     do_debug("@disk_dir: #{@disk_dir.inspect} @tmp_dir: #{@tmp_dir}")
-    #     fp.gsub!("@disk_dir", @disk_dir) if fp.include?("@disk_dir") && @disk_dir
-    #     fp.gsub!("@tmp_dir", @tmp_dir) if fp.include?("@tmp_dir") && @tmp_dir
-    #   end
-    #   fp
-    # end
-    
-    # James: this method is used to apply the system token into the config
-    def apply_sys_token(config)
-      if config.is_a?(String)
-        # this is where we substitute token with values
-        config.gsub!("@disk_dir", @disk_dir) if config.include?("@disk_dir") && @disk_dir
-        config.gsub!("@tmp_dir", @tmp_dir) if config.include?("@tmp_dir") && @tmp_dir
-      elsif config.is_a?(Hash)
-        config.each_pair do |key, value|
-          config[key] = apply_sys_token(value)
-        end
-        #END each_pair
+    def resolve_sys_dir(fp)
+      if fp
+        fp.gsub!("@this_node", @disk_dir) if fp.include?("@this_node") && @disk_dir
+        fp.gsub!("@this_server", @tmp_dir) if fp.include?("@this_server") && @tmp_dir
       end
-      config
+      fp
     end
-    #END apply_sys_token
+    
+    # # James: this method is used to apply the system token into the config
+    # def apply_sys_token(config)
+    #   if config.is_a?(String)
+    #     # this is where we substitute token with values
+    #     config.gsub!("@disk_dir", @disk_dir) if config.include?("@disk_dir") && @disk_dir
+    #     config.gsub!("@tmp_dir", @tmp_dir) if config.include?("@tmp_dir") && @tmp_dir
+    #   elsif config.is_a?(Hash)
+    #     config.each_pair do |key, value|
+    #       config[key] = apply_sys_token(value)
+    #     end
+    #     #END each_pair
+    #   end 
+    #   config
+    # end
+    # #END apply_sys_token
+    
     
   end
   
